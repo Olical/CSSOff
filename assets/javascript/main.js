f6037e20c61c430dcda5fce50ff24c29593a7cdd
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-21 13:32:23 +0100

    Removed the JavaScript and CSS that controled the current class

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,33 +6,7 @@
 	 * Run when the DOM is ready
 	 **/
 	function main() {
-		// Initialise variables
-		var elements = {
-				navigation: $$('nav a'),
-				currentNavigation: document.getElement('nav a[href=' + document.location.hash + ']')
-			};
 		
-		// If there is a current navigation already, apply the class
-		if(elements.currentNavigation && elements.currentNavigation.get('href') !== '#') {
-			elements.currentNavigation.addClass('current');
-		}
-		
-		// Set up navigation
-		elements.navigation.each(function(el) {
-			// On click, remove the class from the current item
-			// And add it to the element as long as it has a hash link
-			el.addEvent('click', function() {
-				if(elements.currentNavigation) {
-					elements.currentNavigation.removeClass('current');
-				}
-				
-				elements.currentNavigation = el;
-				
-				if(el.get('href') !== '#') {
-					el.addClass('current');
-				}
-			});
-		});
 	}
 	
 	// Call the main function when the DOM is ready
