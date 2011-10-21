2d22d802303db852c3009b40d0f345b112f35473
Oliver Caldwell <oliver@flowdev.co.uk>
2011-10-21 12:45:42 +0100

    Set up the navigation in the javascript file along with the sandbox and dom ready event

---
+++ assets/javascript/main.js
@@ -0,0 +1,35 @@
+// Sandboxing wrapper
+(function($, $$) {
+	/**
+	 * Main function for the page
+	 * Invokes all other methods
+	 * Run when the DOM is ready
+	 **/
+	function main() {
+		// Initialise variables
+		var elements = {
+				navigation: $$('nav a'),
+				currentNavigation: null
+			};
+		
+		// Set up navigation
+		elements.navigation.each(function(el) {
+			// On click, remove the class from the current item
+			// And add it to the element as long as it has a hash link
+			el.addEvent('click', function() {
+				if(elements.currentNavigation) {
+					elements.currentNavigation.removeClass('current');
+				}
+				
+				elements.currentNavigation = el;
+				
+				if(el.get('href') !== '#') {
+					el.addClass('current');
+				}
+			});
+		});
+	}
+	
+	// Call the main function when the DOM is ready
+	document.addEvent('domready', main);
+}(document.id, document.getElements));
